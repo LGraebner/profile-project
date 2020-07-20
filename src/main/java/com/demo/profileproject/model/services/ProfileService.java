0606454877c44192cf74fbe5ce6b623c932d2515
Laurent Graebner <laurent.graebner@gmail.com>
2020-07-20 22:49:27 +0200

    add backend caching

--- src/main/java/com/demo/profileproject/model/services/ProfileService.java
+++ src/main/java/com/demo/profileproject/model/services/ProfileService.java
@@ -9,6 +9,10 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,6 +25,7 @@ public class ProfileService {
 
   @Autowired private ModelMapper modelMapper;
 
+  @Cacheable(cacheNames = "all-profiles-cache")
   public List<ProfileDto> getProfiles() {
     log.info("Fetching all profiles");
     return Streams.stream(profileRepository.findAll())
@@ -28,12 +33,16 @@ public class ProfileService {
         .collect(Collectors.toList());
   }
 
+  @Caching(evict = {
+      @CacheEvict(value = "all-profiles-cache", allEntries = true)
+  })
   public ProfileDto createProfile(ProfileDto profileDto) {
     log.info("Creating profile={}", profileDto);
     Profile profile = profileRepository.save(modelMapper.map(profileDto, Profile.class));
     return modelMapper.map(profile, ProfileDto.class);
   }
 
+  @Cacheable(cacheNames = "id-profiles-cache")
   public ProfileDto getProfileById(Long id) {
     log.info("Fetching profile with id={}", id);
     Profile profile =
@@ -44,6 +53,10 @@ public class ProfileService {
     return modelMapper.map(profile, ProfileDto.class);
   }
 
+  @Caching(evict = {
+      @CacheEvict(value = "all-profiles-cache", allEntries = true),
+      @CacheEvict(value = "id-profiles-cache", key = "#id")
+  })
   public ProfileDto updateProfile(Long id, ProfileDto profileDto) {
     log.info("Updating profile with id={}, data={}", id, profileDto);
     Profile profile =
@@ -58,11 +71,19 @@ public class ProfileService {
     return modelMapper.map(profile, ProfileDto.class);
   }
 
+  @Caching(evict = {
+      @CacheEvict(value = "all-profiles-cache", allEntries = true),
+      @CacheEvict(value = "id-profiles-cache", key = "#id")
+  })
   public void deleteProfileById(Long id) {
     log.info("Deleting profile with id={}", id);
     profileRepository.deleteById(id);
   }
 
+  @Caching(evict = {
+      @CacheEvict(value = "all-profiles-cache", allEntries = true),
+      @CacheEvict(value = "id-profiles-cache", key = "#id")
+  })
   public void updateImageName(Long id, String imageName) {
     log.info("Updating imageName for profileId {} to {}", id, imageName);
     Profile profile = profileRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile does not exist: " + id));
